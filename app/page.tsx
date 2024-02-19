bc1e9fb365b3e98dbac7913f608feca04f160f9c
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2024-02-19 22:37:19 +0200

    create values setion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/containers/home-page/hero-section";
 import NewArrivals from "@/containers/home-page/new-arrivals";
+import Values from "@/containers/home-page/values";
 
 export default function Home() {
   return (
     <div className="max-container">
       <Hero />
       <NewArrivals />
+      <Values />
     </div>
   );
 }
